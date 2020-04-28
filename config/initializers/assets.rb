8ab7dc231c238a99131fb24879d9205d3ac5e4c4
Barry Walker <barrywalker@gmail.com>
2020-04-27 23:16:08 -0400

    Clean up asset pipeline for stylesheets.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,6 +12,5 @@ Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( style.css plugins/highlightjs/highlight.pack.min.js
-  plugins/common/common.min js/custom.min js/settings js/styleSwitcher plugins/highlightjs/highlight.pack.min
-  plugins/chart.js/Chart.bundle.min.js js/plugins-init/chartjs-init.js )
+Rails.application.config.assets.precompile += %w( plugins/highlightjs/highlight.pack.min.js
+  plugins/common/common.min js/custom.min js/settings js/styleSwitcher )
